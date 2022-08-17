583036d8318f19e0d8eaf99a4b2d29417796d998
Silvan Sievers <silvan.sievers@unibas.ch>
2022-08-17 10:22:34 +0200

    [weak-stubborn-sets] scripts

--- experiments/weak-stubborn-sets/2022-08-05-blind-compliant-relaxed.py
+++ experiments/weak-stubborn-sets/2022-08-05-blind-compliant-relaxed.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+from collections import defaultdict
 from itertools import chain, combinations
 import os
 from pathlib import Path
@@ -9,6 +10,7 @@ from lab.environments import LocalEnvironment, BaselSlurmEnvironment
 from lab.reports import Attribute, arithmetic_mean, geometric_mean
 
 from downward.reports.compare import ComparativeReport
+from downward.reports.scatter import ScatterPlotReport
 
 import common_setup
 from common_setup import IssueConfig, IssueExperiment
@@ -103,5 +105,170 @@ exp.add_report(
     outfile=report_file,
 )
 exp.add_step(f'publish-{report_name}', subprocess.call, ['publish', report_file])
+
+unsolvable = ['miconic-fulladl:f21-3.pddl', 'miconic-fulladl:f30-2.pddl', 'mystery:prob04.pddl', 'mystery:prob05.pddl', 'mystery:prob07.pddl', 'mystery:prob08.pddl', 'mystery:prob12.pddl', 'mystery:prob16.pddl', 'mystery:prob18.pddl', 'mystery:prob21.pddl', 'mystery:prob22.pddl', 'mystery:prob23.pddl', 'mystery:prob24.pddl']
+
+def filter_unsolvable_tasks(run):
+    return "{}:{}".format(run['domain'], run['problem']) not in unsolvable
+
+domain_groups_optimal = {
+'agricola-opt18-strips': 'agricola',
+'airport': 'airport', # identity
+'assembly': 'assembly', # identity
+'barman-opt11-strips': 'barman',
+'barman-opt14-strips': 'barman',
+'blocks': 'blocks', # identity
+'caldera-opt18-adl': 'caldera',
+'caldera-split-opt18-adl': 'caldera-split',
+'cavediving-14-adl': 'cavediving',
+'childsnack-opt14-strips': 'childsnack',
+'citycar-opt14-adl': 'citycar',
+'data-network-opt18-strips': 'data-network',
+'depot': 'depot', # identity
+'driverlog': 'driverlog', # identity
+'elevators-opt08-strips': 'elevators',
+'elevators-opt11-strips': 'elevators',
+'floortile-opt11-strips': 'floortile',
+'floortile-opt14-strips': 'floortile',
+'freecell': 'freecell', # identity
+'ged-opt14-strips': 'ged',
+'grid': 'grid', # identity
+'gripper': 'gripper', # identity
+'hiking-opt14-strips': 'hiking',
+'logistics00': 'logistics',
+'logistics98': 'logistics',
+'maintenance-opt14-adl': 'maintenance',
+'miconic': 'miconic', # identity
+'miconic-fulladl': 'miconic',
+'miconic-simpleadl': 'miconic',
+'movie': 'movie', # identity
+'mprime': 'mprime', # identity
+'mystery': 'mystery', # identity
+'nomystery-opt11-strips': 'nomystery',
+'nurikabe-opt18-adl': 'nurikabe',
+'openstacks': 'openstacks', # identity
+'openstacks-opt08-adl': 'openstacks',
+'openstacks-opt08-strips': 'openstacks',
+'openstacks-opt11-strips': 'openstacks',
+'openstacks-opt14-strips': 'openstacks',
+'openstacks-strips': 'openstacks',
+'optical-telegraphs': 'optical-telegraphs', # identity
+'organic-synthesis-opt18-strips': 'organic-synthesis',
+'organic-synthesis-split-opt18-strips': 'organic-synthesis-split',
+'parcprinter-08-strips': 'parcprinter',
+'parcprinter-opt11-strips': 'parcprinter',
+'parking-opt11-strips': 'parking',
+'parking-opt14-strips': 'parking',
+'pathways': 'pathways', # identity
+'pathways-noneg': 'pathways',
+'pegsol-08-strips': 'pegsol',
+'pegsol-opt11-strips': 'pegsol',
+'petri-net-alignment-opt18-strips': 'petri-net-alignment',
+'philosophers': 'philosophers', # identity
+'pipesworld-notankage': 'pipesworld-notankage', # pipesworld?
+'pipesworld-tankage': 'pipesworld-tankage', # pipesworld?
+'psr-large': 'psr',
+'psr-middle': 'psr',
+'psr-small': 'psr',
+'rovers': 'rovers', # identity
+'satellite': 'satellite', # identity
+'scanalyzer-08-strips': 'scanalyzer',
+'scanalyzer-opt11-strips': 'scanalyzer',
+'schedule': 'schedule', # identity
+'settlers-opt18-adl': 'settlers',
+'snake-opt18-strips': 'snake',
+'sokoban-opt08-strips': 'sokoban',
+'sokoban-opt11-strips': 'sokoban',
+'spider-opt18-strips': 'spider',
+'storage': 'storage', # identity
+'termes-opt18-strips': 'termes',
+'tetris-opt14-strips': 'tetris',
+'tidybot-opt11-strips': 'tidybot',
+'tidybot-opt14-strips': 'tidybot',
+'tpp': 'tpp', # identity
+'transport-opt08-strips': 'transport',
+'transport-opt11-strips': 'transport',
+'transport-opt14-strips': 'transport',
+'trucks': 'trucks', # identity
+'trucks-strips': 'trucks',
+'visitall-opt11-strips': 'visitall',
+'visitall-opt14-strips': 'visitall',
+'woodworking-opt08-strips': 'woodworking',
+'woodworking-opt11-strips': 'woodworking',
+'zenotravel': 'zenotravel', # identity
+}
+
+def domain_as_category(run1, run2):
+    return run1['domain']
+
+FORMAT="png"
+
+class FilterDomainsWithLargePruningDifference:
+    def __init__(self, algorithms_of_interest):
+        self.algorithms_of_interest = algorithms_of_interest
+        self.domain_to_problem_to_pruning = defaultdict(dict)
+        self.computed = False
+
+    def collect_data(self, run):
+        if run['algorithm'] in self.algorithms_of_interest:
+            if run['problem'] not in self.domain_to_problem_to_pruning[run['domain']]:
+                self.domain_to_problem_to_pruning[run['domain']][run['problem']] = []
+            self.domain_to_problem_to_pruning[run['domain']][run['problem']].append(run.get('ss_pruning_ratio', None))
+        return run
+
+    def compute_interesting_domains(self, run):
+        if not self.computed:
+            self.computed = True
+            self.interesting_domains = set()
+            for domain, problem_to_pruning in self.domain_to_problem_to_pruning.items():
+                for pruning in problem_to_pruning.values():
+                    if len(pruning) != 2:
+                        print(pruning)
+                    assert len(pruning) == 2
+                    # criterion of the paper
+                    # if pruning[0] is not None and pruning[1] is not None and abs(pruning[0] - pruning[1]) > 0.1:
+                    # any pruning
+                    if pruning[0] is not None and pruning[1] is not None and (pruning[0] > 0.0 or pruning[1] > 0.0):
+                        self.interesting_domains.add(domain)
+            # print("interesting domains:", sorted(self.interesting_domains))
+        return run
+
+    def interesting_domains_as_category(self, run1, run2):
+        if run1['domain'] in self.interesting_domains:
+            return domain_groups_optimal[run1['domain']]
+        return ''
+
+for ss_comparison in [
+    ('strong', 'weak'),
+    ('strong', 'compliant'),
+    ('strong', 'compliant_relaxed'),
+    ('weak', 'compliant'),
+    ('weak', 'compliant_relaxed'),
+    ('compliant', 'compliant_relaxed'),
+]:
+    ss1 = ss_comparison[0]
+    ss2 = ss_comparison[1]
+    for heuristic in ['blind']:
+        interesting_filter = FilterDomainsWithLargePruningDifference(
+            algorithms_of_interest=[
+                f'{REVISION}-{heuristic}-simple-{ss1}-min0.0',
+                f'{REVISION}-{heuristic}-simple-{ss2}-min0.0']
+        )
+
+        exp.add_report(
+            ScatterPlotReport(
+                attributes=[ss_pruning_ratio],
+                format=FORMAT,
+                filter=[filter_unsolvable_tasks,interesting_filter.collect_data,interesting_filter.compute_interesting_domains],
+                filter_algorithm=[
+                    f'{REVISION}-{heuristic}-simple-{ss1}-min0.0',
+                    f'{REVISION}-{heuristic}-simple-{ss2}-min0.0',
+                ],
+                show_missing=False,
+                get_category=interesting_filter.interesting_domains_as_category,
+                scale='linear',
+            ),
+            outfile=os.path.join(exp.eval_dir, f'scatter-pruningratio-{heuristic}-simple-{ss1}-{ss2}.{FORMAT}')
+        )
 
 exp.run_steps()
